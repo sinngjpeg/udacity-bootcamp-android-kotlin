92dcace9d60438f6872c39139b453e42511160e9
Ingrid Silva <ingridsilvatec@outlook.com>
2021-02-05 18:33:04 -0300

    finish project

--- ColorMyViews/app/src/main/java/com/sinngjpeg/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/sinngjpeg/colormyviews/MainActivity.kt
@@ -25,7 +25,10 @@ class MainActivity : AppCompatActivity() {
                 binding.boxThreeText,
                 binding.boxFourText,
                 binding.boxFiveText,
-                binding.constraintLayout
+                binding.constraintLayout,
+                binding.redButton,
+                binding.yellowButton,
+                binding.greenButton
             )
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -43,6 +46,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
