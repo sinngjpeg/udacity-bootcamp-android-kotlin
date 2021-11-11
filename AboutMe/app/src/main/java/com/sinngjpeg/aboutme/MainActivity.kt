b98121072c6fe81fdf8a709840cb32bec66a443b
unknown <Ingrid Silva>
2021-11-11 15:56:51 -0300

    refector the project

--- AboutMe/app/src/main/java/com/sinngjpeg/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/sinngjpeg/aboutme/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.sinngjpeg.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.sinngjpeg.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val myName: MyName = MyName("Ingrid Castro","")
+    private val myName: MyName = MyName("Ingrid Silva", "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.nameText.text = myName.name
-
 
         binding.doneButton.setOnClickListener {
             addNickname(it)
@@ -28,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     private fun addNickname(view: View) {
 
         binding.apply {
-            //nicknameText.text = binding.nicknameEdit.text
+            // nicknameText.text = binding.nicknameEdit.text
             myName?.nickname = nicknameEdit.text.toString()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
